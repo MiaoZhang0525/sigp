16f1ca65c7934f9683b70efbef931dc1f16bba11
Zilong Tan <eric.zltan@gmail.com>
2018-03-20 16:39:24 -0400

    remove lambda in opt

--- trainKernel.m
+++ trainKernel.m
@@ -9,33 +9,32 @@ function res = trainKernel(X,y,m,k)
 %
 % Copyright (c) 2018 Zilong Tan (ztan@cs.duke.edu)
 
-lambda = optimizableVariable('lambda', [1e-5,1],'Transform','log');
-band   = optimizableVariable('band', [10,1e3],'Transform','log');
+band   = optimizableVariable('band', [1,1e3],'Transform','log');
 
-minfn = @(z) objfn(k,X,y,m,z.lambda,z.band);
+minfn = @(z) objfn(k,X,y,m,z.band);
 
-res = bayesopt(minfn,[lambda,band],'IsObjectiveDeterministic',true,...
+res = bayesopt(minfn,band,'IsObjectiveDeterministic',true,...
     'AcquisitionFunctionName','expected-improvement-plus', ...
     'ExplorationRatio',0.5,'MaxObjectiveEvaluations',30);
 
 end
 
-function loss = objfn(k,X,y,m,lambda,band)
+function loss = objfn(k,X,y,m,band)
 c = cvpartition(y,'kFold',k);
-fun = @(xT,yT,xt,yt) crossnlp(xT,yT,xt,yt,m,lambda,band);
+fun = @(xT,yT,xt,yt) crossnlp(xT,yT,xt,yt,m,band);
 nlp = crossval(fun,X,y,'partition',c);
 nlp = nlp(:);
-loss = median(nlp);
+loss = mean(nlp);
 end
 
-function nlp = crossnlp(xT,yT,xt,yt,m,lambda,band)
-hyp = sigp(xT,yT,m,'kparam',band,'efn','ker','lambda',lambda);
+function nlp = crossnlp(xT,yT,xt,yt,m,band)
+hyp = sigp(xT,yT,m,'kparam',band,'efn','ker','lambda',1e-2);
 
 % Use the L2 loss function (useful for classification)
-%nlp = norm(yt-hyp.f(xt))/sqrt(size(yt,1));
+nlp = norm(yt-hyp.f(xt))/sqrt(size(yt,1));
 
 %Use the NLPD loss function (useful for regression)
-[yp,ys2] = hyp.f(xt);
-nlp = log(2*pi)/2 + sum(log(ys2)/2 + (yp-yt).^2./ys2/2)/size(yp,1);
+%[yp,ys2] = hyp.f(xt);
+%nlp = log(2*pi)/2 + sum(log(ys2)/2 + (yp-yt).^2./ys2/2)/size(yp,1);
 
 end
