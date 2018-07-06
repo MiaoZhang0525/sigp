fdf0257b3d49faa8b6d1eb5f63929aea23fc183a
Zilong Tan <eric.zltan@gmail.com>
2018-07-06 08:52:51 -0400

    more kenrel functions

--- sigp.m
+++ sigp.m
@@ -6,7 +6,7 @@ function hyp = sigp(X,y,m,varargin)
 %    m specifies the rank of the desired RKHS
 %
 % Optional:
-%    MaxIter is the max number of EM iterations
+%    maxiter is the max number of EM iterations
 %    tol specifies the minimum change in the objective value between two
 %        consecutive EM iterations.
 %    ns  is the maximum number of slices, each slice corresponds to a range of y.
@@ -14,9 +14,8 @@ function hyp = sigp(X,y,m,varargin)
 %    eta is small postive number used to improve the condition of A
 %    lambda is the mean function regularization parameter
 %    efn specifies the error function, 'lin' - linear, 'ker' - kernel Ridge
-%    kfn gives the kernel function, default is RBF
-%    kpar is the kfn parameter. For builtin RBF, kpar is the bandwidth
-%    SDR - specifies whether to use SDR initialization, default is true
+%    meankfn/covkfn give the mean and covariance kernel functions,
+%       default are linear and rational quadratic
 %
 % Returns the model struct hyp. Some important members are
 %    hyp.f  is the fitted target function f: X -> [Y,varY]
@@ -26,43 +25,60 @@ function hyp = sigp(X,y,m,varargin)
 % Copyright (c) 2018 Zilong Tan (ztan@cs.duke.edu)
 
 hyp = struct();
-n = size(X,1);
+[n,p] = size(X);
 
 opt = inputParser;
-opt.addParameter( 'MaxIter', 50, @(x) floor(x) > 0 );
+opt.addParameter( 'maxiter', 30, @(x) floor(x) > 0 );
 opt.addParameter( 'tol', 1e-4, @(x) floor(x) >= 0);
-opt.addParameter( 'ns', max(m+1,floor(n/10)), @(x) floor(x) > 0 & floor(x) < n/2);
+opt.addParameter( 'ns', 0, @(x) floor(x) > 1 & floor(x) <= n/2); % 1 for auto select
 opt.addParameter( 'eta', 1e-11, @(x) floor(x) >= 0);
 opt.addParameter( 'lambda', 1e-5, @(x) floor(x) >= 0);
 opt.addParameter( 'efn', 'ker', @(x) strcmp(x,'lin')|strcmp(x,'ker'));
-opt.addParameter( 'kfn', @sigp_rbf, @(x) feval(x,[]));
-opt.addParameter( 'kparam', 1, @(x) true);
-opt.addParameter( 'SDR', true, @(x) islogical(x));
+opt.addParameter( 'meankfn',  @sigp_lin, @(x) feval(x) >= 0); % used only if opt.efn == 'ker'
+opt.addParameter( 'covkfn',   @sigp_rq,  @(x) feval(x) >= 0);
+opt.addParameter( 'meankpar', [], @(x) true);
+opt.addParameter( 'covkpar',  [1 1], @(x) true);
+opt.addParameter( 'showlik',  false, @(x) islogical(x));
+opt.addParameter( 'normalize',true,  @(x) islogical(x));
 opt.parse(varargin{:});
-opt = opt.Results;
+opt = opt.Results;    
 
-% Center the data (do not normalize the variance)
-% Need adjustment for prediction
-hyp.Xmu = mean(X);
-X = X - hyp.Xmu;
+hyp.opt = opt;
 
 if strcmp(opt.efn,'lin')
     use_ker_efn = false;
+    opt.meankfn = @sigp_lin; % override meankfn if using linear mean
 else
     use_ker_efn = true;
 end
+
+% Center the data (do not normalize the variance)
+% Need adjustment for prediction
+if opt.normalize
+    hyp.Xmu  = mean(X);
+    hyp.Xstd = max(std(X)*sqrt(p),1e-12);
+else
+    hyp.Xmu  = zeros(1,size(X,2));
+    hyp.Xstd = ones(1,size(X,2));
+end
+X = X - hyp.Xmu;
+X = X ./ hyp.Xstd;
 
 % Partition the data by response range
 % This works for both regression and classification
 [y,idx] = sort(y,'ascend');
 X = X(idx,:);
 [~,nun] = unique(y);
+ns = opt.ns;
+if ns == 0 % auto select
+    ns = min(length(nun),floor(n/2));
+end
 nun = [nun(2:end)-nun(1:end-1); n+1-nun(end)];
-if length(nun) <= opt.ns
+if length(nun) <= ns
     csz = nun;
 else
     csz(1,1) = 0;
-    sz = n/opt.ns;
+    sz = n/ns;
     i = 1;
     for j = 1:length(nun)
         if csz(i,1) >= sz
@@ -73,40 +89,51 @@ else
     end
 end
 
-kfn = @(X,Z,param) feval(opt.kfn,X,Z,param);
-K = kfn(X,[],opt.kparam);
-% Compute SDR matrices
-A = zeros(n,n);
-pos = cumsum([1;csz]);
-for i = 1:length(csz)
-    idx = pos(i):pos(i+1)-1;
-    A(idx,:) = K(idx,:) - mean(K(idx,:));
-end
-C = K - mean(K);  C = C'*C;
-A = A'*A;  A(1:n+1:end) = A(1:n+1:end) + opt.eta;
+meankfn = @(X,Z,param) feval(opt.meankfn,X,Z,param);
+covkfn  = @(X,Z,param) feval(opt.covkfn,X,Z,param);
+MK = meankfn(X,[],opt.meankpar);
+CK = covkfn(X,[],opt.covkpar);
 
-if opt.SDR
-    % Initialize W with the SDR basis
-    [W,~,~] = eigs(C,A,m);
+if m > 0
+    % Compute RKHS dimension reduction matrices
+    A = zeros(n,n);
+    pos = cumsum([1;csz]);
+    for i = 1:length(csz)
+        idx = pos(i):pos(i+1)-1;
+        A(idx,:) = CK(idx,:) - mean(CK(idx,:));
+    end
+    C = MK; C = C - mean(C); C = C'*C;
+    A = A'*A;  A(1:n+1:end) = A(1:n+1:end) + opt.eta;
+    [W,~] = eigs(C,A,m);
+    W = W./sqrt(sum(W.^2));
 else
-    % Initialize W randomly
-    W = randn(n,m);
+    % use the full covariance kernel
+    m = n;
+    W = eye(n);
 end
 
 if use_ker_efn
-    efn = @(varargin) sigp_efn_ker(y,K,varargin{:});
+    efn = @(varargin) sigp_efn_ker(y,MK,varargin{:});
 else
     efn = @(varargin) sigp_efn_lin(y,X,varargin{:});
 end
 
 % Initialize other parameters
-s2 = 1;  Sb = eye(m); iSb = Sb;
+s2 = 1; Sb = eye(m); iSb = Sb;
 beta = zeros(m,1);
 err = y;  res = err;
-P = K*W;  PTP = P'*P;
-hyp.W = W./sqrt(sum(W.^2)); hyp.nlp = [];
+P = CK*W;  PTP = P'*P;
+hyp.nlp = [];
 
-for i = 1:opt.MaxIter
+if opt.showlik
+    figure;
+    hold on;
+    title('Negative Log-Likelihood');
+    xlabel('Iteration');
+    ylabel('NLL');
+end
+
+for i = 1:opt.maxiter
     % Use square root for better numerical conditions
     V = compV(P*sqrtm(Sb),s2);
     % Negative log-likelihood
@@ -116,6 +143,7 @@ for i = 1:opt.MaxIter
     % Fit mean function
     alp = efn(V,opt.lambda);
     err = efn(alp);
+    if opt.showlik, plot(hyp.nlp,'b-','linewidth',2); end
     if length(hyp.nlp) > 1 && hyp.nlp(end-1) - hyp.nlp(end) < opt.tol
         break;
     end
@@ -126,10 +154,11 @@ for i = 1:opt.MaxIter
     Sb  = diag(diag(Sv) + beta.^2);
     iSb = inv(Sb);
     res = err - P*beta;
-    % Fit W and noise variance
+    % Fit the noise variance
     s2 = s2 + (sum(res.^2) - s2^2*trace(V))/n;
 end
 
+hyp.W = W;
 hyp.alpha = alp;
 hyp.beta  = beta;
 hyp.Q = W*sqrtm(Sb);
@@ -138,31 +167,19 @@ hyp.NoiseVar = s2;
 MF = W*beta;
 CF = W*sqrtm(Sv);
 
+hyp.covkfn = @(Z) covkfn((Z-hyp.Xmu)./hyp.Xstd,X,opt.covkpar);
 if strcmp(opt.efn,'ker')
-    hyp.f = @(Z) sigp_pred_ker(kfn(Z-hyp.Xmu,X,opt.kparam), ...
-            @(KZ) -sigp_efn_ker(zeros(size(Z,1),1),KZ,alp), ...
-            MF,CF,s2);
     hyp.mf = @(Z)-sigp_efn_ker(zeros(size(Z,1),1),...
-             kfn(Z-hyp.Xmu,X,opt.kparam),alp);
+                meankfn((Z-hyp.Xmu)./hyp.Xstd,X,opt.meankpar),alp);    
 else
-    hyp.f = @(Z) sigp_pred_lin(kfn(Z-hyp.Xmu,X,opt.kparam), ...
-            -sigp_efn_lin(zeros(size(Z,1),1),Z-hyp.Xmu,alp), ...
-            MF,CF,s2);
-    hyp.mf = @(Z)-sigp_efn_lin(zeros(size(Z,1),1),...
-             Z-hyp.Xmu,alp);
+    hyp.mf = @(Z)-sigp_efn_lin(zeros(size(Z,1),1),(Z-hyp.Xmu)./hyp.Xstd,alp);    
 end
-
-hyp.kfn = @(Z) kfn(Z-hyp.Xmu,X,opt.kparam);
+hyp.f = @(Z) sigp_pred(hyp.covkfn(Z),hyp.mf(Z),MF,CF,s2);
 
 end
 
-function [pmu,pvar] = sigp_pred_ker(KZ,mufn,MF,CF,s2)
-pmu = mufn(KZ) + KZ*MF; % use one reference to KZ to save computation
-pvar = sum((KZ*CF).^2,2) + s2;
-end
-
-function [pmu,pvar] = sigp_pred_lin(KZ,muZ,MF,CF,s2)
-pmu = muZ + KZ*MF;
+function [pmu,pvar] = sigp_pred(KZ,muZ,MF,CF,s2)
+pmu  = muZ + KZ*MF;
 pvar = sum((KZ*CF).^2,2) + s2;
 end
 
@@ -218,22 +235,30 @@ S = svd(X);
 val = sum(log(S));
 end
 
-function K = sigp_poly(X,Z,param)
-if nargin == 0 || isempty(X), K = 2; return, end
-if nargin == 3 && isempty(Z), Z = X; end
-K = (param(1) + X*Z').^param(2); % param(2) must be a positive integer
+function SE = pairwise_sqerr(X,Z)
+sqX = sum(X.^2,2);
+sqZ = sum(Z.^2,2);
+SE = bsxfun(@minus, sqX, (2*X)*Z');
+SE = bsxfun(@plus, sqZ', SE);
+SE = max(SE,0); % may be negative due to rounding errors
 end
 
-function K = sigp_lin(X,Z,band)
-if nargin == 0 || isempty(X), K = 1; return, end
+function K = sigp_poly(X,Z,param)
+if nargin == 0 || isempty(X), K = 2; return, end
 if nargin == 3
-    X = X/band;
+    X = X/param(1);
     if ~isempty(Z)
-        Z = Z/band;
+        Z = Z/param(1);
     else
         Z = X;
     end
 end
+K = (1 + X*Z').^param(2); % param(2) must be a positive integer
+end
+
+function K = sigp_lin(X,Z,param)
+if nargin == 0 || isempty(X), K = 0; return, end
+if nargin == 3 && isempty(Z), Z = X; end
 K = X*Z';
 end
 
@@ -261,9 +286,83 @@ if nargin == 3
         Z = X;
     end
 end
+% basically -pairwise_sqerr(X,Z), negate vectors rather than the matrix
 sqX = -sum(X.^2,2);
 sqZ = -sum(Z.^2,2);
 K = bsxfun(@plus, sqX, (2*X)*Z');
 K = bsxfun(@plus, sqZ', K);
 K = exp(K);
+end
+
+% Rational quadratic kernel
+% k(x,z) = (1 + (x-z)'(x-z)/alpha/band^2)^(-alpha)
+function K = sigp_rq(X,Z,param)
+if nargin == 0 || isempty(X), K = 1; return, end
+if nargin == 3
+    X = X/sqrt(param(1))/param(2);
+    if ~isempty(Z)
+        Z = Z/sqrt(param(1))/param(2);
+    else
+        Z = X;
+    end
+end
+K = (1+pairwise_sqerr(X,Z)).^(-param(1));
+end
+
+
+function K = sigp_sinc(X,Z,band)
+if nargin == 0 || isempty(X), K = 1; return, end
+if nargin == 3
+    X = X/band^2;
+    if ~isempty(Z)
+        Z = Z/band^2;
+    else
+        Z = X;
+    end
+end
+K = sinc(sqrt(pairwise_sqerr(X,Z)));
+end
+
+% Matern v=1/2 kernel, also the Laplace kernel
+function K = sigp_matern12(X,Z,band)
+if nargin == 0 || isempty(X), K = 1; return, end
+if nargin == 3
+    X = X/band^2;
+    if ~isempty(Z)
+        Z = Z/band^2;
+    else
+        Z = X;
+    end
+end
+K = exp(-sqrt(pairwise_sqerr(X,Z)));
+end
+
+% Matern v=3/2 kernel, also the Laplace kernel
+function K = sigp_matern32(X,Z,band)
+if nargin == 0 || isempty(X), K = 1; return, end
+if nargin == 3
+    X = X/band^2;
+    if ~isempty(Z)
+        Z = Z/band^2;
+    else
+        Z = X;
+    end
+end
+SE = sqrt(pairwise_sqerr(X,Z));
+K = (1+SE).*exp(-SE);
+end
+
+% Student-t kernel
+function K = sigp_stud(X,Z,band)
+if nargin == 0 || isempty(X), K = 1; return, end
+if nargin == 3
+    X = X/band^2;
+    if ~isempty(Z)
+        Z = Z/band^2;
+    else
+        Z = X;
+    end
+end
+SE = sqrt(pairwise_sqerr(X,Z));
+K = 1./(1+SE);
 end
---
+++ trainMS.m
@@ -0,0 +1,31 @@
+function res = trainMS(X,y,m,k)
+
+lambda = optimizableVariable('lambda', [1e-5,1e3], 'Transform', 'log');
+mband  = optimizableVariable('mband',  [1e1,1e3], 'Transform', 'log');
+cband  = optimizableVariable('cband',  [1e-1,1e3], 'Transform', 'log');
+
+minfn = @(z) objfn(k,X,y,m,z.lambda,z.mband,z.cband);
+
+res = bayesopt(minfn,[lambda mband cband],'IsObjectiveDeterministic',true,...
+    'AcquisitionFunctionName','expected-improvement-plus', ...
+    'ExplorationRatio',0.5,'MaxObjectiveEvaluations',50);
+
+end
+
+function loss = objfn(k,X,y,m,lambda,mband,cband)
+c = cvpartition(length(y),'kFold',k);
+fun = @(xT,yT,xt,yt) crossnlp(xT,yT,xt,yt,m,lambda,mband,cband);
+nlp = crossval(fun,X,y,'partition',c);
+nlp = nlp(:);
+loss = mean(nlp);
+end
+
+function nlp = crossnlp(xT,yT,xt,yt,m,lambda,mband,cband)
+hyp = sigp(xT,yT,m,'efn','ker',...
+    'meankfn','sigp_matern32','meankpar',mband,...
+    'covkfn', 'sigp_sinc','covkpar',cband,...
+    'lambda',lambda,'normalize',false);
+nlp = norm(yt-hyp.f(xt))/sqrt(size(yt,1));
+%[yp,ys2] = hyp.f(xt);
+%nlp = log(2*pi)/2 + sum(log(ys2)/2 + (yp-yt).^2./ys2/2)/size(yp,1);
+end
